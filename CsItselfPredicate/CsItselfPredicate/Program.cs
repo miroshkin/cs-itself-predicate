57f8edbb14885370d03bca356a0de5a0caa02f4a
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-10-29 13:17:30 +0300

    Init project

---
+++ CsItselfPredicate/CsItselfPredicate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsItselfPredicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
